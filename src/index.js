41396983be103d178586334963f3e0365d05e059
Vlad0703 <73859766+Vlad0703@users.noreply.github.com>
2021-01-28 16:29:13 +0300

    feat: add new array methods

--- src/index.js
+++ src/index.js
@@ -1,12 +1,12 @@
 
 exports.min = function min (array) {
-  return 0;
+   return (typeof array == 'undefined') ? 0 : (array.length == 0 ? 0 : array.sort( (a, b) => a - b )[0]);
 }
 
 exports.max = function max (array) {
-  return 0;
+  return (typeof array == 'undefined') ? 0 : (array.length == 0 ? 0 : array.sort( (a, b) => a - b ).reverse()[0]);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  eturn (typeof array == 'undefined') ? 0 : (array.length == 0 ? 0 : array.reduce((s, c) => s + c, 0)/array.length);
 }
